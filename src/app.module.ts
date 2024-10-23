16c19dabfd86106291219b4636e6b51afdbae8a5
kimmeh <mhko0411@gmail.com>
2024-10-23 22:27:48 +0900

    feat: add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    EmailModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    CacheModule.register({
+      ttl: 2 * 60 * 1000,
+      max: 100,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
